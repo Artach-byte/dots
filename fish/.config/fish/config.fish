1d928ce1d5923b23bdc0288b0e8991c8c0f5c659
Joseph Hansen <Joseph.hansen.96@gmail.com>
2022-06-20 01:27:01 -0700

    readded fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set -gx EDITOR nvim
+  # Aliases for LSD
+    alias ls='lsd'
+    alias l='ls -l'
+    alias la='ls -a'
+    alias lla='ls -la'
+    alias lt='ls --tree'
+    # lazygit
+    alias lg='lazygit'
+    # back directory
+    alias ..='cd ..'
+    # file Managers
+    alias x='xplr'
+end
