ed232708c3b566df370fc98e74c0de9c461615fa
Joseph Hansen <Joseph.hansen.96@gmail.com>
2022-06-20 02:13:44 -0700

    updated doom emacs config

--- doom.d/.doom.d/config.el
+++ doom.d/.doom.d/config.el
@@ -16,7 +16,7 @@
 ;; + `doom-variable-pitch-font'
 ;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
 ;;   presentations or streaming.
-(setq doom-font(font-spec :family "Cascadia Code" :size 15))
+(setq doom-font(font-spec :family "FiraCode Nerd Font" :size 12))
 ;;
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
@@ -27,7 +27,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-dracula)
+(setq doom-theme 'doom-nord)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
