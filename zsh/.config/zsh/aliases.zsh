6788010806ec9a658edbe816f05e09d09bbc09e3
Joseph Hansen <Joseph.hansen.96@gmail.com>
2022-07-11 03:13:37 -0700

    removed uneeded aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -8,17 +8,11 @@
 # Aliases for ZSH
 alias zshreload='source ~/.zshrc'             # reload ZSH
 
-# lazygit
-alias lg='lazygit'
-
 # back directory
 alias ..='cd ..'
 
 # file Managers
 alias x='xplr'
-
-# vscodium
-alias code='codium'
 
 # exa
 # alias exa='exa --long --header --git --all --icons --grid'
