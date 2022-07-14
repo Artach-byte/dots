ecaa3ac1153d151ee8b15915f39527c423d35cc6
Joseph Hansen <Joseph.hansen.96@gmail.com>
2022-07-13 18:04:39 -0700

    added git aliases to zsh

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -14,5 +14,25 @@ alias ..='cd ..'
 # file Managers
 alias x='xplr'
 
-# exa
-# alias exa='exa --long --header --git --all --icons --grid'
+#git Aliases
+alias g="git"
+alias gst='git status'
+alias gc='git commit'
+alias ga='git add'
+alias gpom='git pull origin main'
+alias gpu='git push'
+alias gpuom='git push origin main'
+alias gd='git diff'
+alias gch='git checkout'
+alias gnb='git checkout -b'
+alias gac='git add . && git commit'
+alias grs='git restore --staged .'
+alias gre='git restore'
+alias gr='git remote'
+alias gcl='git clone'
+alias glg="git log --graph --abbrev-commit --decorate --format=format:'%C(bold green)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold yellow)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
+alias gt='git ls-tree -r main --name-only'
+alias grm='git remote'
+alias gb='git branch'
+alias gm='git merge'
+alias gf='git fetch'
