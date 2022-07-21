5a8e667ee86a0ad0a4f82ba96b447d0a73156554
Joseph Hansen <Joseph.hansen.96@gmail.com>
2022-07-20 21:22:00 -0700

    updated doom emacs

--- doom.d/.doom.d/config.el
+++ doom.d/.doom.d/config.el
@@ -16,7 +16,7 @@
 ;; + `doom-variable-pitch-font'
 ;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
 ;;   presentations or streaming.
-(setq doom-font(font-spec :family "JetBrainsMono Nerd Font" :size 12))
+(setq doom-font(font-spec :family "JetBrainsMono Nerd Font" :size 13))
 ;;
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
