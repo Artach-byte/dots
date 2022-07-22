51dd1a654208029b4bffc8d84e75180634067eb6
Joseph Hansen <Joseph.hansen.96@gmail.com>
2022-07-21 19:32:10 -0700

    removed unused aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,10 +1,3 @@
-# Aliases for LSD
-# alias ls='lsd'
-# alias l='ls -l'
-# alias la='ls -a'
-# alias lla='ls -la'
-# alias lt='ls --tree'
-#
 # Aliases for ZSH
 alias zshreload='source ~/.zshrc' # reload ZSH
 
