cf95b767516d511e6043d113cc5adf2ed61d639e
Joseph Hansen <Joseph.hansen.96@gmail.com>
2022-07-25 02:29:18 -0700

    added config for todo-comments

--- config/.config/nvim/lua/plugins/init.lua
+++ config/.config/nvim/lua/plugins/init.lua
@@ -189,11 +189,7 @@ return packer.startup(function(use)
         "folke/todo-comments.nvim",
         requires = "nvim-lua/plenary.nvim",
         config = function()
-            require("todo-comments").setup {
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
-            }
+            require "plugins.configs.todo-comments"
         end,
     }
 
