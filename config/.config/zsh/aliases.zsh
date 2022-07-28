3f0ee09a886773a0330eb247479884980375e901
Joseph Hansen <Joseph.hansen.96@gmail.com>
2022-07-28 00:08:09 -0700

    added alias for lazygit

--- config/.config/zsh/aliases.zsh
+++ config/.config/zsh/aliases.zsh
@@ -4,6 +4,9 @@ alias zshreload='source ~/.zshrc' # reload ZSH
 # back directory
 alias ..='cd ..'
 
+# lazy git
+alias lg='lazygit'
+
 # file Managers
 alias x='xplr'
 alias xcd='cd "$(xplr --print-pwd-as-result)"'
